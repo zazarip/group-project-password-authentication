7f4f8778ec113b179d0c52d9558697cfc795bdd8
zazarip <106441151+zazarip@users.noreply.github.com>
2022-07-16 14:10:23 +0800

    passserver.py
    zariff part

---
+++ passserver.py
@@ -0,0 +1,29 @@
+import socket
+import os
+import threading
+import hashlib
+
+
+# Create Socket (TCP) Connection
+ServerSocket = socket.socket(family = socket.AF_INET, type = socket.SOCK_STREAM) 
+host = '127.0.0.1'
+port = 1233
+ThreadCount = 0
+try:
+    ServerSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Waitiing for a Connection..')
+ServerSocket.listen(5)
+HashTable = {}
+
+# Function : For each client 
+def threaded_client(connection):
+    connection.send(str.encode('ENTER USERNAME : ')) # Request Username
+    name = connection.recv(2048)
+    connection.send(str.encode('ENTER PASSWORD : ')) # Request Password
+    password = connection.recv(2048)
+    password = password.decode()
+    name = name.decode()
+    password=hashlib.sha256(str.encode(password)).hexdigest() # Password hash using SHA256
